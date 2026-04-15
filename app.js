2c129c537e1e785a2139fd2abaef678a91c763fa
root <root@ip-172-31-27-14.ec2.internal>
2026-04-15 18:32:30 +0000

    fix flash error

--- app.js
+++ app.js
@@ -66,6 +66,8 @@ passport.deserializeUser(User.deserializeUser());
 // ================= GLOBAL =================
 
 app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
     res.locals.currentUser = req.user;
     next();
 });
